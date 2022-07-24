d4a410cda0762202ec7ab752e9b42934b92eae90
Zander Baker <zanderbaker100@gmail.com>
2022-07-23 18:29:55 -0700

    Add force option in Init
    [Class]: Add Force option in Init to override existing project

--- KanBanApp/Commands/Init.cs
+++ KanBanApp/Commands/Init.cs
@@ -7,10 +7,24 @@ public class Init : CommandBase
 {
     [Option("-d|--dir <DirectoryPath>", CommandOptionType.SingleValue)]
     public string? DirectoryPath { get; set; }
+    
+    [Option("-f|--force", CommandOptionType.NoValue)]
+    public bool Force { get; set; }
 
     protected override int Execute()
     {
         var target = PathResolver.RelativeOrAbsoluteFilePath(DirectoryPath);
+
+        if (ProjectInterface.ExistsIn(target))
+        {
+            if (!Force)
+            {
+                WriteOutputLine("A kba project already exists in this directory. Use --force to overwrite.");
+
+                return 1;
+            }
+        }
+        
         var board = new ProjectInterface(target);
         
         board.Write();
