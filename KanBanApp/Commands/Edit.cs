cc19ffaf38588b7943648c1eed8968c39f19a41a
Zander Baker <zanderbaker100@gmail.com>
2022-07-25 17:59:28 -0700

    Create edit command (empty)

---
+++ KanBanApp/Commands/Edit.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KanBanApp.Commands;
+
+public class Edit : CommandBase
+{
+    [Argument(0, "Path")]
+    [Required]
+    public string Path { get; set; }
+
+    protected override int Execute()
+    {
+        return 0;
+    }
+}
